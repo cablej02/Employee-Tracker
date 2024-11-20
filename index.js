99d8b3d887a4d362d1f6ed8f43680f10220ea937
Jason Cable <cablej02@gmail.com>
2024-11-19 20:50:14 -0600

    added error handling to all functions

--- index.js
+++ index.js
@@ -22,212 +22,222 @@ const viewAllEmployees = async () => {
         FROM employee e 
             LEFT OUTER JOIN "role" r ON e.role_id = r.id
             LEFT OUTER JOIN department d ON d.id = r.department_id 
-            LEFT OUTER JOIN employee e2 ON e2.id = e.manager_id
+            LEFT OUTER JOIN employee e2 ON e2.id = e.manager_id;
     `;
 
-    const { rows } = await pool.query(query);
-
-    console.table(rows);
-
-    startInquirer();
+    try {
+        const { rows } = await pool.query(query);
+        console.table(rows);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const addEmployee = async () => {
     //get the manager ids from the database
     const managerQuery = `SELECT id, first_name, last_name FROM employee;`;
-    const { rows: managers } = await pool.query(managerQuery);
-
     //get the role ids from the database
     const roleQuery = `SELECT id, title FROM "role";`;
-    const { rows: roles } = await pool.query(roleQuery);
-
-    const { first_name, last_name, role_id, manager_id } = await inquirer.prompt([
-        {
-            type: "input",
-            name: "first_name",
-            message: "Enter the employee's first name:",
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "Enter the employee's last name:",
-        },
-        {
-            type: "list",
-            name: "role_id",
-            message: "Choose the employee's role:",
-            choices: roles.map(role => ({
-                name: role.title,
-                value: role.id,
-            })),
-        },
-        {
-            type: "list",
-            name: "manager_id",
-            message: "Choose the employee's manager:",
-            choices: [
-                { name: "None", value: null },
-                ...managers.map(manager => ({
-                    name: `${manager.first_name} ${manager.last_name}`,
-                    value: manager.id,
-                })),
-            ],
-        },
-    ]);
 
     const query = `
         INSERT INTO employee (first_name, last_name, role_id, manager_id)
         VALUES ($1, $2, $3, $4);
     `;
 
-    await pool.query(query, [first_name, last_name, role_id, manager_id]);
+    try {
+        const { rows: managers } = await pool.query(managerQuery);
+        const { rows: roles } = await pool.query(roleQuery);
 
-    console.log("Employee added!");
+        const { first_name, last_name, role_id, manager_id } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "first_name",
+                message: "Enter the employee's first name:",
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "Enter the employee's last name:",
+            },
+            {
+                type: "list",
+                name: "role_id",
+                message: "Choose the employee's role:",
+                choices: roles.map(role => ({
+                    name: role.title,
+                    value: role.id,
+                })),
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                message: "Choose the employee's manager:",
+                choices: [
+                    { name: "None", value: null },
+                    ...managers.map(manager => ({
+                        name: `${manager.first_name} ${manager.last_name}`,
+                        value: manager.id,
+                    })),
+                ],
+            },
+        ]);
 
-    startInquirer();
+        await pool.query(query, [first_name, last_name, role_id, manager_id]);
+
+        console.log("Employee added!");
+
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const viewAllRoles = async () => {
     const query = `
         SELECT r.id, r.title, d."name" AS department, r.salary::FLOAT AS salary
         FROM "role" r 
-            JOIN department d ON r.department_id = d.id 
+            JOIN department d ON r.department_id = d.id;
     `;
 
-    const { rows } = await pool.query(query);
-
-    console.table(rows);
-
-    startInquirer();
+    try {
+        const { rows } = await pool.query(query);
+        console.table(rows);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const addRole = async () => {
     //get the department ids from the database
     const departmentQuery = `SELECT id, name FROM department;`;
-    const { rows: departments } = await pool.query(departmentQuery);
-
-    const { title, salary, department_id } = await inquirer.prompt([
-        {
-            type: "input",
-            name: "title",
-            message: "Enter the name of the role:",
-        },
-        {
-            type: "input",
-            name: "salary",
-            message: "Enter the salary of the role:",
-        },
-        {
-            type: "list",
-            name: "department_id",
-            message: "Choose the role's department:",
-            choices: departments.map(department => ({
-                name: department.name,
-                value: department.id,
-            })),
-        },
-    ]);
 
     const query = `
         INSERT INTO role (title, salary, department_id)
         VALUES ($1, $2, $3);
     `;
 
-    await pool.query(query, [title, salary, department_id]);
+    try {
+        const { rows: departments } = await pool.query(departmentQuery);
 
-    console.log("Role added!");
+        const { title, salary, department_id } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "title",
+                message: "Enter the name of the role:",
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Enter the salary of the role:",
+            },
+            {
+                type: "list",
+                name: "department_id",
+                message: "Choose the role's department:",
+                choices: departments.map(department => ({
+                    name: department.name,
+                    value: department.id,
+                })),
+            },
+        ]);
 
-    startInquirer();
+        await pool.query(query, [title, salary, department_id]);
+        console.log("Role added!");
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const viewAllDepartments = async () => {
     const query = `SELECT * FROM department;`;
 
-    const { rows } = await pool.query(query);
-
-    console.table(rows);
-
-    startInquirer();
+    try {
+        const { rows } = await pool.query(query);
+        console.table(rows);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const addDepartment = async () => {
-    const { name } = await inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Enter the department name:",
-        },
-    ]);
-
     const query = `
         INSERT INTO department (name)
         VALUES ($1);
     `;
-    await pool.query(query, [name]);
 
-    console.log("Department added!");
+    try {
+        const { name } = await inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Enter the department name:",
+            },
+        ]);
 
-    startInquirer();
+
+        await pool.query(query, [name]);
+        console.log("Department added!");
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const updateEmployeeManager = async () => {
     //get the employee ids from the database
     const employeeQuery = `SELECT id, first_name, last_name FROM employee;`;
-    const { rows: employees } = await pool.query(employeeQuery);
-
-    const { employee_id, manager_id } = await inquirer.prompt([
-        {
-            type: "list",
-            name: "employee_id",
-            message: "Choose the employee to update:",
-            choices: employees.map(employee => ({
-                name: `${employee.first_name} ${employee.last_name}`,
-                value: employee.id,
-            })),
-        },
-        {
-            type: "list",
-            name: "manager_id",
-            message: "Choose the employee's new manager:",
-            choices: [
-                { name: "None", value: null },
-                ...employees.map(manager => ({
-                    name: `${manager.first_name} ${manager.last_name}`,
-                    value: manager.id,
-                })),
-            ],
-        },
-    ]);
 
     const query = `
         UPDATE employee
         SET manager_id = $2
-        WHERE id = $1;
+        WHERE id = $1
     `;
 
-    await pool.query(query, [employee_id, manager_id]);
+    try {
+        const { rows: employees } = await pool.query(employeeQuery);
 
-    console.log("Employee manager updated!");
+        const { employee_id, manager_id } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "employee_id",
+                message: "Choose the employee to update:",
+                choices: employees.map(employee => ({
+                    name: `${employee.first_name} ${employee.last_name}`,
+                    value: employee.id,
+                })),
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                message: "Choose the employee's new manager:",
+                choices: [
+                    { name: "None", value: null },
+                    ...employees.map(manager => ({
+                        name: `${manager.first_name} ${manager.last_name}`,
+                        value: manager.id,
+                    })),
+                ],
+            },
+        ]);
 
-    startInquirer();
+        await pool.query(query, [employee_id, manager_id]);
+        console.log("Employee manager updated!");
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const viewEmployeesByManager = async () => {
-
-    const managerQuery = `SELECT DISTINCT e2.id, e2.first_name, e2.last_name FROM employee e JOIN employee e2 ON e.manager_id = e2.id;`;
-    const { rows: managers } = await pool.query(managerQuery);
-
-    const { manager_id } = await inquirer.prompt([
-        {
-            type: "list",
-            name: "manager_id",
-            message: "Choose the manager:",
-            choices: managers.map(manager => ({
-                name: `${manager.first_name} ${manager.last_name}`,
-                value: manager.id,
-            })),
-        },
-    ]);
+    const managerQuery = `
+        SELECT DISTINCT e2.id, e2.first_name, e2.last_name 
+        FROM employee e 
+            JOIN employee e2 ON e.manager_id = e2.id;
+    `;
 
     const query = `
         SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary::FLOAT AS salary
@@ -236,28 +246,32 @@ const viewEmployeesByManager = async () => {
             JOIN department d ON d.id = r.department_id
         WHERE e.manager_id = $1;
     `;
-    const { rows } = await pool.query(query, [manager_id]);
+    
+    try {
+        const { rows: managers } = await pool.query(managerQuery);
 
-    console.table(rows);
+        const { manager_id } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "manager_id",
+                message: "Choose the manager:",
+                choices: managers.map(manager => ({
+                    name: `${manager.first_name} ${manager.last_name}`,
+                    value: manager.id,
+                })),
+            },
+        ]);
 
-    startInquirer();
+        const { rows } = await pool.query(query, [manager_id]);
+        console.table(rows);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const viewEmployeesByDepartment = async () => {
     const departmentQuery = `SELECT * FROM department;`;
-    const { rows: departments } = await pool.query(departmentQuery);
-
-    const { department_id } = await inquirer.prompt([
-        {
-            type: "list",
-            name: "department_id",
-            message: "Choose the department:",
-            choices: departments.map(department => ({
-                name: department.name,
-                value: department.id,
-            })),
-        },
-    ]);
 
     const query = `
         SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary::FLOAT AS salary,
@@ -266,34 +280,37 @@ const viewEmployeesByDepartment = async () => {
                 ELSE CONCAT(e2.first_name, ' ', e2.last_name) 
             END AS manager
         FROM employee e
-            LEFT OUTER JOIN "role" r ON e.role_id = r.id
-            LEFT OUTER JOIN department d ON r.department_id = d.id 
+            JOIN "role" r ON e.role_id = r.id
+            JOIN department d ON r.department_id = d.id 
             LEFT OUTER JOIN employee e2 ON e2.id = e.manager_id
         WHERE d.id = $1;
     `;
 
-    const { rows } = await pool.query(query, [department_id]);
+    try {
+        const { rows: departments } = await pool.query(departmentQuery);
 
-    console.table(rows);
+        const { department_id } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "department_id",
+                message: "Choose the department:",
+                choices: departments.map(department => ({
+                    name: department.name,
+                    value: department.id,
+                })),
+            },
+        ]);
 
-    startInquirer();
+        const { rows } = await pool.query(query, [department_id]);
+        console.table(rows);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const viewDepartmentBudget = async () => {
     const departmentQuery = `SELECT * FROM department;`;
-    const { rows: departments } = await pool.query(departmentQuery);
-
-    const { department_id } = await inquirer.prompt([
-        {
-            type: "list",
-            name: "department_id",
-            message: "Choose the department:",
-            choices: departments.map(department => ({
-                name: department.name,
-                value: department.id,
-            })),
-        },
-    ]);
 
     const query = `
         SELECT d.name, SUM(r.salary::FLOAT) AS total_utilized_budget
@@ -304,11 +321,27 @@ const viewDepartmentBudget = async () => {
         GROUP BY d.name;
     `;
 
-    const { rows } = await pool.query(query, [department_id]);
+    try {
+        const { rows: departments } = await pool.query(departmentQuery);
 
-    console.log(`Total utilized budget for ${rows[0].name}: $${rows[0].total_utilized_budget}`);
+        const { department_id } = await inquirer.prompt([
+            {
+                type: "list",
+                name: "department_id",
+                message: "Choose the department:",
+                choices: departments.map(department => ({
+                    name: department.name,
+                    value: department.id,
+                })),
+            },
+        ]);
 
-    startInquirer();
+        const { rows } = await pool.query(query, [department_id]);
+        console.log(`Total utilized budget for ${rows[0].name}: $${rows[0].total_utilized_budget}`);
+        startInquirer();
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 const deleteDepartment = async () => {}
